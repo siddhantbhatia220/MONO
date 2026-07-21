e3ae6386eb4371ec5dd0a6bcbad386c625c9c339
Siddhant Bhatia <siddhantbhatia220@gmail.com>
2026-07-21 11:41:59 +0530

    added workspacesController to isolate workspace config responses

---
+++ src/lib/api/controllers/workspacesController.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+import { getWorkspaceTemplates } from '../services/workspacesService'
+
+export async function getWorkspaces() {
+  const templates = getWorkspaceTemplates()
+  return NextResponse.json({
+    templatesCount: templates.length,
+    templates,
+  })
+}
