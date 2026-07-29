69860ff557673819fd2eb55097f7a1f4453b27dd
Siddhant Bhatia <siddhantbhatia220@gmail.com>
2026-07-29 15:06:07 +0530

    added NestJS ItemsModule wiring items dependencies

---
+++ server/src/items/items.module.ts
@@ -0,0 +1,14 @@
+/**
+ * MONO — NestJS Items Module
+ */
+import { Module } from '@nestjs/common'
+import { ItemsController } from './items.controller'
+import { ItemsService } from './items.service'
+import { PrismaService } from '../prisma/prisma.service'
+
+@Module({
+  controllers: [ItemsController],
+  providers: [ItemsService, PrismaService],
+  exports: [ItemsService],
+})
+export class ItemsModule {}
