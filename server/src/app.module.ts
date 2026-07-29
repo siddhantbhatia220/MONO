b79d8f7b9265d9b9e9ac3789da3b4d830282af88
Siddhant Bhatia <siddhantbhatia220@gmail.com>
2026-07-29 14:42:23 +0530

    added NestJS root application module app.module.ts

---
+++ server/src/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * MONO — NestJS Root Application Module
+ */
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
